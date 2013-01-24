a715256b82b79e4abab2308742a78c977f4f7a78
Ivan Teoh <ivan@pretaweb.com>
2013-01-24 17:23:51 +1100

    Move rolemap to config.py

--- collective/pfg/signup/config.py
+++ collective/pfg/signup/config.py
@@ -1,7 +1,10 @@
 """config -- shared values"""
-
+from Products.CMFCore.permissions import setDefaultRoles
 
 PROJECTNAME = 'collective.pfg.signup'
 ADD_PERMISSIONS = {
     'SignUpAdapter': 'collective.pfg.signup: Add SignUpAdapter',
     }
+setDefaultRoles(ADD_PERMISSIONS['SignUpAdapter'],
+                ('Manager', 'Owner', 'Contributor', 'Site Administrator')
+)
