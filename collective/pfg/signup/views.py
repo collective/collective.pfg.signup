bb6466f62ae8e6302b92d70f98e9d12279756138
Michael Davis <m.r.davis@me.com>
2013-04-29 13:23:57 +0100

    Add the group to the view and remove the management view class

--- collective/pfg/signup/views.py
+++ collective/pfg/signup/views.py
@@ -14,7 +14,6 @@ class UserApproverView(BrowserView):
 
     def __init__(self, context, request):
         super(UserApproverView, self).__init__(context, request)
-        print "UserApproverView: init"
         self.results = []
 
     def result_data(self):
@@ -24,6 +23,7 @@ class UserApproverView(BrowserView):
     def result_columns(self):
         return [{ "sTitle": "User Name" },
                 { "sTitle": "Full Name" },
+                { "sTitle": "Group" },
                 { "sTitle": "Email" },
                 { "sTitle": "Approve" },
                 { "sTitle": "Reject" },
@@ -59,101 +59,9 @@ class UserApproverView(BrowserView):
             reject_button = '<a href="' + link + '/reject_user?userid=' + key + '">Reject button</a>'
             results.append([value['username'],
                             value['fullname'],
+                            value['user_group'],
                             value['email'],
                             approve_button,
                             reject_button])
         self.results = results
-        return self.index()
-
-class UserManagementView(BrowserView):
-    # TODO The form in user_management_view need to have Plone csrf protection
-    # not sure this is the right way for passing data in Plone
-    # datatable plugin is the right tool for this case?
-    # redirect in the view is right way to do it?
-    # TODO: Browser view need to have custom permission that only able to view
-    # by Authenticated user. For now, it is public.
-    index = ViewPageTemplateFile("templates/user_approver_view.pt")
-
-    def __init__(self, context, request):
-        super(UserManagementView, self).__init__(context, request)
-        print "UserManagementView: init"
-
-    def __call__(self):
-        # aq_inner is needed in some cases like in the portlet renderers
-        # where the context itself is a portlet renderer and it's not on the
-        # acquisition chain leading to the portal root.
-        # If you are unsure what this means always use context.aq_inner
-        print "UserManagementView: call"
-        context = self.context.aq_inner
-        portal_membership = getToolByName(context, 'portal_membership')
-
-        if portal_membership.isAnonymousUser():
-            # Return target URL for the site anonymous visitors
-            # TODO return error??
-            return self.index()
-
-        if 'results' not in self.request:
-            # TODO return error??
-            return self.index()
-
-        if 'submit' not in self.request:
-            # TODO return error??
-            return self.index()
-
-        results_string = self.request['results']
-        is_approve = self.request['submit']
-
-        results = []
-        for result in results_string.split('&'):
-            key_hash, _ = result.split("=")
-            if key_hash not in context.waiting_list:
-                continue
-
-            key_id = context.waiting_list[key_hash]
-
-            # need to verified?
-            key_hash_original = hashlib.sha224(key_id).hexdigest()
-
-            if key_hash_original != key_hash:
-                # TODO raise error?
-                continue
-
-            if is_approve == 'approve' or is_approve == 'reject':
-                del context.waiting_list[key_hash]
-                results.append(key_id)
-
-        user_id = portal_membership.getAuthenticatedMember().getId()
-        portal_groups = getToolByName(context, 'portal_groups')
-        user_groups = portal_groups.getGroupsByUserId(user_id)
-
-        # get the user approval list based on groups
-        for user_group in user_groups:
-            user_group_name = user_group.getName()
-            if user_group_name not in context.waiting_by_approver:
-                continue
-            all_results = waiting_by_approver[user_group_name]
-
-            for key in results:
-
-                # TODO remove the waiting_list
-                if key not in all_results:
-                    continue
-
-                this_result = all_results[key]
-                email = this_result['email']
-
-                if is_approve == 'approve':
-                    # create an account:
-                    self.create_member(self.request, this_result, True, user_group)
-                    del context.waiting_by_approver[user_group_name][key]
-                elif is_approve == 'reject':
-                    # TODO: create reject email template
-                    mail_body = u"Sorry your user registration has been " \
-                                u"rejected. Thank you."
-                    send_email(mail_body,
-                               self.portal.getProperty('email_from_address'),
-                               email,
-                               'Rejected Email')
-                    del context.waiting_by_approver[user_group_name][key]
-
         return self.index()
