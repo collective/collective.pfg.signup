b5f1fedc4e2b02dbc10021d88c1b2d12852fb1fd
Ivan Teoh <ivan.teoh@gmail.com>
2015-07-16 16:31:00 +1000

    Update the version.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.0'
+version = '1.1'
 short_description = "A PloneFormGen adapter that saves signup form"
 long_description = (
     open('README.md').read()
